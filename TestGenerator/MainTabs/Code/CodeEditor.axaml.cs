229fa9654d49fc40e1c9d1a774cfcde4dce1e28e
SergeiKrivko <sergik.krivko@gmail.com>
2024-10-05 23:33:12 +0300

    Подсветка кода

--- TestGenerator/MainTabs/Code/CodeEditor.axaml.cs
+++ TestGenerator/MainTabs/Code/CodeEditor.axaml.cs
@@ -1,20 +1,39 @@
 ﻿using System;
 using System.IO;
 using Avalonia.Controls;
+using AvaloniaEdit;
 using AvaloniaEdit.Document;
+using AvaloniaEdit.TextMate;
+using TextMateSharp.Grammars;
 
 namespace TestGenerator.MainTabs.Code;
 
 public partial class CodeEditor : UserControl
 {
+    private readonly TextMate.Installation _textMateInstallation;
+    private readonly RegistryOptions _registryOptions;
+
     public CodeEditor()
     {
         InitializeComponent();
+        
+        _registryOptions = new RegistryOptions(ThemeName.DarkPlus);
+
+        //Initial setup of TextMate.
+        _textMateInstallation = Editor.InstallTextMate(_registryOptions);
+
+        //Here we are getting the language by the extension and right after that we are initializing grammar with this language.
+        //And that's all 😀, you are ready to use AvaloniaEdit with syntax highlighting!
+        _textMateInstallation.SetGrammar(
+            _registryOptions.GetScopeByLanguageId(_registryOptions.GetLanguageByExtension(".cs").Id));
     }
 
     public void Open(string fileName)
     {
-        using StreamReader reader = new(fileName);
-        Editor.Text = reader.ReadToEnd();
+        _textMateInstallation.SetGrammar(null);
+        Editor.Document = new TextDocument(File.ReadAllText(fileName));
+        _textMateInstallation.SetGrammar(_registryOptions.GetScopeByLanguageId(
+            _registryOptions.GetLanguageByExtension("." + fileName.Split('.')[^1]).Id
+        ));
     }
 }
