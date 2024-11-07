fc5bf477a35af050655efd0e0b4c5a1274d4952d
SergeiKrivko <sergik.krivko@gmail.com>
2024-11-08 00:24:31 +0300

    Add main tab template

---
+++ TestGenerator.Plugin/Templates/MainTabTemplate/NewMainTab.cs
@@ -0,0 +1,13 @@
+﻿using TestGenerator.Shared.Types;
+
+namespace Testenerator.Plugin.Namespace;
+
+public partial class NewMainTab : MainTab
+{
+    public NewMainTab()
+    {
+        TabKey = "NewMainTab";
+        TabName = "NewMainTab";
+        InitializeComponent();
+    }
+}
