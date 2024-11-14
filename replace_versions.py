15ac3a38484e03a9d3f0241c91f465ae61e3b9f4
SergeiKrivko <sergik.krivko@gmail.com>
2024-11-14 19:58:47 +0300

    Add build application pipeline

---
+++ replace_versions.py
@@ -0,0 +1,13 @@
+﻿with open("version.txt") as f:
+    version = f.read().strip()
+
+
+for file in ["Releases/win-x64/setup.iss", "Releases/mac-x64/TestGenerator.app/Contents/Info.plist", 
+             "Releases/linux-x64/debpkg/usr/share/applications/TestGenerator.desktop"]:
+    with open(file, 'r') as f:
+        text = f.read()
+        
+    text = text.replace("{AppVersion}", version)
+    
+    with open(file, 'w') as f:
+            f.write(text)
---
+++ upload_release.py
@@ -0,0 +1,29 @@
+﻿import sys
+import os
+
+from github import Github, Auth
+
+
+path, arch = sys.argv[1], sys.argv[2]
+
+
+with open("version.txt", 'r', encoding='utf-8') as f:
+    version = f.read().strip().replace('\ufeff', '')
+
+print(f"Version = {repr(version)}")
+
+
+# using an access token
+auth = Auth.Token(os.getenv("GITHUB_TOKEN"))
+
+# Public Web GitHub
+g = Github(auth=auth)
+
+repo = g.get_repo('SergeiKrivko/TestGenerator-cs')
+
+release = repo.get_latest_release()
+print(repr(release.tag_name), version)
+if release.tag_name != "v" + version:
+    release = repo.create_git_release("v" + version, f"Version {version}", '')
+
+release.upload_asset(path, name=f"gptchat_{version}_{arch}.{path.split('.')[-1]}")
