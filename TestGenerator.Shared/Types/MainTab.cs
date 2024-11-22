984b76f1a05329c06be7178938497b3b2fbf9bf6
SergeiKrivko <sergik.krivko@gmail.com>
2024-11-22 14:49:20 +0300

    fis shared lib

--- TestGenerator.Shared/Types/MainTab.cs
+++ TestGenerator.Shared/Types/MainTab.cs
@@ -4,8 +4,8 @@ namespace TestGenerator.Shared.Types;
 
 public class MainTab : UserControl
 {
-    public virtual string TabKey { get; } = new Guid().ToString();
-    public virtual string TabName { get; } = "";
+    public virtual string TabKey { get; protected set; } = new Guid().ToString();
+    public virtual string TabName { get; protected set; } = "";
 
     public virtual int TabPriority { get; } = 10;
 }
