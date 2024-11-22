629884df82ff9ee01cf4ad001c5f9fa1eb9d0c0d
SergeiKrivko <sergik.krivko@gmail.com>
2024-11-22 14:50:44 +0300

    fix shared lib

--- TestGenerator.Shared/Types/SideTab.cs
+++ TestGenerator.Shared/Types/SideTab.cs
@@ -5,8 +5,8 @@ namespace TestGenerator.Shared.Types;
 public abstract class SideTab : UserControl
 {
     public virtual string TabKey { get; } = new Guid().ToString();
-    public abstract string TabName { get; protected set; }
-    public abstract string TabIcon { get; protected set; }
+    public abstract string TabName { get; }
+    public abstract string TabIcon { get; }
 
     public virtual int TabPriority { get; } = 10;
 }
