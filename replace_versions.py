025b9bc6b897ec8d297a268cf8caf81577036e16
SergeiKrivko <sergik.krivko@gmail.com>
2024-12-10 15:26:58 +0300

    deb package

--- replace_versions.py
+++ replace_versions.py
@@ -4,7 +4,7 @@
 
 for file in ["Releases/win-x64/setup.iss", "Releases/mac-x64/TestGenerator.app/Contents/Info.plist", 
              "Releases/linux-x64/debpkg/usr/share/applications/TestGenerator.desktop",
-             "TestGenerator.Core/Services/AppService.cs"]:
+             "TestGenerator.Core/Services/AppService.cs", "Releases/linux-x64/debpkg/DEBIAN/control"]:
     with open(file, 'r') as f:
         text = f.read()
         
