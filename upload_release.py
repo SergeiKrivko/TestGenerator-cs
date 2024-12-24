3af5e1336c1294b6b6ec7f91b46fae1fe4ffb0f4
SergeiKrivko <sergik.krivko@gmail.com>
2024-12-24 21:59:09 +0300

    fix upload script

--- upload_release.py
+++ upload_release.py
@@ -47,8 +47,7 @@ with zipfile.ZipFile('temp.zip', 'w', compression=zipfile.ZIP_DEFLATED) as zipf:
 
 resp = requests.post(
     f"{URL}/releases/upload?version={version}&runtime={runtime}",
-    data={'Files': [m['filename'] for m in file_models]},
-    files={'Zip': open('temp.zip', 'rb')},
+    data={'files': [m['filename'] for m in file_models], 'zip': open('temp.zip', 'rb')},
     headers={'Authorization': f'Bearer {os.getenv("TESTGEN_TOKEN")}'}
 )
 print('Upload:', resp.status_code)
